48f3875abc607c6f1fbb352e2c573ca13bccfbad
sonhmle <sonhmle@kms-technology.com>
2022-04-20 08:35:52 +0700

    Fix file path with env

--- test.sh
+++ test.sh
@@ -6,6 +6,6 @@ printf "KOBITON EXECUTE TEST PLUGIN"
 printf "\n=====================================================================================\n\n"
 
 # change the name of app to run based on your system (app_darwin for macOS, app_linux for Linux, app_windows for Windows)
-chmod +x ${{ github.action_path }}/app-to-run/app_linux
+chmod +x $GITHUB_ACTION_PATH/app-to-run/app_linux
 
-${{ github.action_path }}/app-to-run/app_linux
+$GITHUB_ACTION_PATH/app-to-run/app_linux
